e4675bba250a45f0d7146e6bd325be2a84c0386c
Dennismwendwa <dennismusembi2@.com>
2023-02-14 06:18:19 -0800

    commitMessage

---
+++ 0x03-shell_variables_expansions/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                                             
+echo "What is the commit message?"
+read commitMessage                                                                                                                              
+git status                                                                                                                     
+git add .                                                                                                                      
+git status                                                                                                                     
+git commit -m commitMessage                                                                                                    
+git push origin master                                                                                                         
+                         i
