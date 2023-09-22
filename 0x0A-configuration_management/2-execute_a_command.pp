3f64a9d58da856b2a6a50ad89fc97c8ad6331862
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-22 14:33:27 +0300

    correct linter

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # this manifest uses pkill to kill the killmenow process
 
 
-exec { 'kills process':
-  command     => 'pkill killmenow',
-  path        =>   ['/bin', '/usr/bin'],
-  provider    => 'shell',
+exec {'kills process':
+  command  => 'pkill killmenow',
+  path     => ['/bin', '/usr/bin'],
+  provider => 'shell',
 }
