93c0bad28f32e290c91454cd09341e6cc94fd036
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-23 07:50:13 +0300

    ssh and puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# configuring ssh client using puppet
+
+
+exec { 'echo':
+  path     => ['/bin', '/usr/bin'],
+  command  => 'echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns  => [0, 1],
+}
