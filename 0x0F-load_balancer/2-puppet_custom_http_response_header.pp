d673ffdb824044c68d7158b9b898985eb1bbf1df
Dennismwendwa <dennismusembi2@gmail.com>
2023-10-02 12:39:35 +0300

    puppet manifest

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+# Setting custom http header for nginx
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name_;',
+}
+
+service { 'nginx':
+  ensure => running,
+}
