03ea62ea5d38b1d76346b9c988339599fb9aefdc
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-10-24 05:25:17 +0300

    updated json file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,36 +6,19 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 1:
-        print("Usage: python3 3-dictionary_of_list_of_dictionaries.py")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    users_url = url + "users"
+    todos_url = url + "todos"
 
-    def get_data(res, par=None):
-        """get user"""
-        url = "https://jsonplaceholder.typicode.com/"
-        url += res
-
-        if par:
-            url += ("?" + par[0] + "=" + par[1])
-
-        re = requests.get(url)
-
-        re = re.json()
-        return re
-
-    data_e = {}
-    users = get_data("users")
-    for user in users:
-        user_id = user["id"]
-        data_e.update({user_id: []})
-        tasks_by_user = get_data("todos", ("userId", str(user_id)))
-        for task in tasks_by_user:
-            data_e[user_id].append({"username": user["username"],
-                                    "task": task["title"],
-                                    "completed": task["completed"]})
-
+    users = requests.get(users_url).json()
 
     with open("todo_all_employees.json", mode="w") as json_file:
-        json.dump(data_e, json_file)  # added indent=4 better format
-
-    print("Data exported to todo_all_employees.json")
+        json.dump({
+            user.get("id"): [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            } for todo in requests.get(todos_url,
+                                       params={"userId": user.get("id")}
+                                       ).json()]
+            for user in users}, json_file)
