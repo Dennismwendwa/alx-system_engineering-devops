944ced44960c002147c4b55fef8b1ebcd4b6ecc2
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-07 13:14:58 +0300

    pycode style error

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""sending request to reddit api"""
+"""This script sends request to reddit api"""
 import requests
 
 
 def top_ten(subreddit):
     """this function send get request to reddit api"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-
     headers = {"User-Agent": "MyBot"}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
 
-        for k, post in enumerate(posts[:10]):
+        for post in posts[:10]:
             title = post["data"]["title"]
             print(f"{title}")
     else:
