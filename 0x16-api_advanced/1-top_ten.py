25c20b19dd773a0cda75f746d7ccc3f1632be8e0
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-07 14:46:41 +0300

    pycode style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,15 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """this function send get request to reddit api"""
+    """
+    this function send get request to reddit api
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "MyBot"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
-
         for post in posts[:10]:
             title = post["data"]["title"]
             print(f"{title}")
