062f18b2731518e72bc8b4e77bbb8ebe2d54e214
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-07 14:57:24 +0300

    pycode style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""This script sends get request to reddit api and counts the titles return"""
+"""
+This script sends get request to reddit api and counts the titles return
+"""
+
 import requests
 
 
 def top_ten(subreddit):
     """
-    this function send get request to reddit api
+    This function send get request to reddit api
     """
+    
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "MyBot"}
     response = requests.get(url, headers=headers, allow_redirects=False)
