a1a2f7871090904e23cc60220b2eae5eb73bbc09
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-07 12:02:44 +0300

    sending api request

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this script sends request to reddit api"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {"User-Agent": "MyBot"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        sunscribers = data["data"]["subscribers"]
+        return sunscribers
+    else:
+        
+        return 0
